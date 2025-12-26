41f4ab51be2f6a112347aff01ed71c3f650cb968
Jugal Balaji <jugalbalaji97@gmail.com>
2025-12-26 14:23:44 +0530

    add keyboard support and fix backspace bug

--- script.js
+++ script.js
@@ -17,11 +17,18 @@ const BUTTON_MAP = {
     "":"", //for updating display when operator is not assigned
 };
 
+// let INV_BUTTON_MAP = {}
+// for(item in BUTTON_MAP) INV_BUTTON_MAP[BUTTON_MAP[item]] = item;
+
+const NUMBERS = "0123456789.";
+const OPERATORS = "/*-+"
+const OTHER_VALID_KEYS = ["=", "Enter", "Backspace", "Delete"];
+
 const OPERATION_MAP = {
-    "add": (a, b) => a + b,
-    "subtract": (a, b) => a - b,
-    "divide": (a, b) => a / b,
-    "multiply": (a, b) => a * b,
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "/": (a, b) => a / b,
+    "*": (a, b) => a * b,
 };
 
 let expression = {
@@ -80,7 +87,7 @@ function updateDisplay(isCalculate=false) {
         primaryDisplayText = roundDecimals(answer);
     }
     else {
-        primaryDisplayText = roundDecimals(expression.operand1) + " " + BUTTON_MAP[expression.operator] + " " + roundDecimals(expression.operand2);
+        primaryDisplayText = roundDecimals(expression.operand1) + " " + expression.operator + " " + roundDecimals(expression.operand2);
     }
     primaryDisplay.textContent = truncateDisplayText(primaryDisplayText, 13);
 }
@@ -103,53 +110,89 @@ function clear() {
     secondaryDisplay.textContent = "";
 }
 
-numbers.addEventListener("click", (e)=>{
-    if(expression.isResult && !expression.operator) clear();
-
-    let operand = (!expression.operator) ? "operand1": "operand2";
-
-    if (expression[operand].length <= 9) {
-        if (e.target.id == "decimal") {
-            if (!expression[operand].includes(".")) {
-                if(!expression[operand]) expression[operand] += "0" + BUTTON_MAP[e.target.id];
-                else expression[operand] += BUTTON_MAP[e.target.id];
-            }
-        }
-        else expression[operand] += BUTTON_MAP[e.target.id];
-    }
-
-    updateDisplay();
-    e.stopPropagation();
-});
-
-operators.addEventListener("click", (e)=>{
-    if (expression.operand1 && !expression.operand2) expression.operator = e.target.id;
-    else if (expression.operand1 && expression.operand2) {
-        calculate();
-        updateDisplay(isCalculate=true);
-        expression.operator = e.target.id;
-    }
-
-    updateDisplay();
-    e.stopPropagation();
-});
-
-calculateButton.addEventListener("click", (e) => {
+function calculateEvent () {
     if (expression.operator && expression.operand2) {
         calculate();
         updateDisplay(isCalculate=true);
     }
+}
 
-    e.stopPropagation();
-});
-
-clearButton.addEventListener("click", (e) => clear());
-
-deleteButton.addEventListener("click", (e) => {
-    if (!expression.isResult) {
+function backspace () {
+     if (!expression.isResult) {
          if (expression.operand2) expression.operand2 = expression.operand2.slice(0, -1);
         else if (expression.operator) expression.operator = "";
         else if (expression.operand1) expression.operand1 = expression.operand1.slice(0, -1);
     }
     updateDisplay();
+}
+
+function operatorEntry (operator) {
+    expression.isResult = false;
+    if (expression.operand1 && !expression.operand2) expression.operator = operator;
+    else if (expression.operand1 && expression.operand2) {
+        calculate();
+        updateDisplay(isCalculate=true);
+        expression.operator = operator;
+    }
+    updateDisplay();
+}
+
+function numberEntry (number) {
+    if(expression.isResult && !expression.operator) clear();
+
+    let operand = (!expression.operator) ? "operand1": "operand2";
+
+    if (expression[operand].length <= 9) {
+        if (number == ".") {
+            if (!expression[operand].includes(".")) {
+                if(!expression[operand]) expression[operand] += "0" + number;
+                else expression[operand] += number;
+            }
+        }
+        else expression[operand] += number;
+    }
+
+    updateDisplay();
+}
+
+numbers.addEventListener("click", (e)=>{
+    e.target.blur(); // unfocus button to avoid accidental trigger on pressing "Enter" key
+    numberEntry(BUTTON_MAP[e.target.id]);
+    e.stopPropagation();
 });
+
+operators.addEventListener("click", (e)=>{
+    e.target.blur(); // unfocus button to avoid accidental trigger on pressing "Enter" key
+    operatorEntry(BUTTON_MAP[e.target.id]);
+    e.stopPropagation();
+});
+
+calculateButton.addEventListener("click", (e) => {
+    e.target.blur();
+    calculateEvent()
+    e.stopPropagation();
+});
+
+clearButton.addEventListener("click", (e) => {
+    e.target.blur();
+    clear();
+    e.stopPropagation();
+});
+
+deleteButton.addEventListener("click", (e) => {
+    e.target.blur();
+    backspace();
+    e.stopPropagation();
+});
+
+//Keyboard support
+document.addEventListener("keydown", (e)=>
+{
+    if (NUMBERS.includes(e.key) || OPERATORS.includes(e.key) || OTHER_VALID_KEYS.includes(e.key)) {
+        if (e.key == "=" || e.key =="Enter") calculateEvent();
+        else if (e.key == "Backspace" || e.key == "Delete") backspace();
+        else if (OPERATORS.includes(e.key)) operatorEntry(e.key);
+        else if (NUMBERS.includes(e.key)) numberEntry(e.key);
+    }
+    e.stopPropagation();
+})
